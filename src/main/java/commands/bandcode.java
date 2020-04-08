997ce4f3851f8b8c64450ec1677674994f26873a
kingbobsta <30960992+kingbobsta@users.noreply.github.com>
2020-04-07 23:36:00 -0700

    Update bandcode.java

--- src/main/java/commands/bandcode.java
+++ src/main/java/commands/bandcode.java
@@ -35,7 +35,7 @@ public class bandcode extends Command {
             Request request = new Request.Builder()
                     .url(urlToUse)
                     .method("GET", null)
-                    .addHeader("X-eBirdApiToken", "idnbs3hmk5lf")
+                    .addHeader("X-eBirdApiToken", "{{eBird API TOKEN}}")
                     .build();
             Response res = client.newCall(request).execute();
             if (!res.isSuccessful()) {
